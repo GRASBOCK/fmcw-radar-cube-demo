a6426ac92c6831733b4ae749818b258258145a79
Richard Körner <grasbock@gmail.com>
2025-12-13 14:26:38 +0100

    change name of project

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './fmcw_radar_cube_demo.js',
+  './fmcw_radar_cube_demo_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
